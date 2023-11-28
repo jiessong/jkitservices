2be0927f26aad073c4114291bac740c66d144aa1
Jie <jies_2408@126.com>
2023-11-28 16:40:44 +1100

    First upload

---
+++ jklitservices/src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Stats from "./Stats";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    Testimonials,
+    Hero,
+    Stats,
+}
